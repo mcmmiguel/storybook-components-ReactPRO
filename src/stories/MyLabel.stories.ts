98de980e6cf3c81be5ba1f6927d8e08ee5e9c03c
Miguel Cobian <miguelcobian99@gmail.com>
2023-11-03 11:37:16 -0600

    Configure to save in NPM

--- src/stories/MyLabel.stories.ts
+++ src/stories/MyLabel.stories.ts
@@ -1,7 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { MyLabel } from '../components/MyLabel';
+import { MyLabel, MyLabelProps } from '../components/MyLabel';
 
-const meta = {
+const meta: Meta<MyLabelProps> = {
     title: 'UI/labels/MyLabel',
     component: MyLabel,
     tags: [
